e38b54030ddb7dd0e620aa3d567315a033ec50a4
dave@netbook <dave@netbook>
2009-12-18 16:11:40 +0000

    Some docs, dedicated to Foobacca.

--- lib/adhd.rb
+++ lib/adhd.rb
@@ -5,6 +5,24 @@ require 'erb'
 require 'ruby-debug'
 require File.dirname(__FILE__) + '/adhd/models'
 
+# Start the server for now by cd'ing into the /lib directory and running the
+# following command:
+#
+# (first node):
+# ruby adhd.rb <node_name> <couchdb_server_url>
+#
+# (second or later node)
+# ruby adhd.rb <node_name> <couchdb_server_url> <management_node_url> <management_node_db>
+#
+# <node_name> is just a string, e.g. "foo".
+# <couchdb_server_url>: the url (including port) for this node's couchdb server
+#   instance, e.g, http://192.168.1.104:5984
+# <management_node_url>: the url of the management node where this node should
+#   initially replicate from, e.g. http://192.168.1.104:5984
+# <management_node_db>: the couchdb management node database, e.g. "bar_node_db"
+
+
+
 node_name = ARGV[0]
 node_url = ARGV[1]
 buddy_server_url = ARGV[2]
