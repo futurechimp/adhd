9e2a0b6113b73e611d371d3351d54e36496af3b5
Manos <manossi@gmail.com>
2009-12-18 18:29:01 +0000

    Make the sync resitant to nodes being down

--- lib/adhd/models.rb
+++ lib/adhd/models.rb
@@ -4,12 +4,21 @@ class NodeDB
   def sync
     # If not, we find out where the management node is and
     # we replicate to the administrative node.
-    if !our_node.is_management
-      management_node = Node.by_is_management.last
-      remote_db = management_node.get_node_db
-      local_node_db.replicate_from(remote_db)
-      # TODO: Manage conflicts here
-      local_node_db.replicate_to(remote_db) 
+    if !our_node.is_management      
+      management_nodes = Node.by_is_management.reverse
+      management_nodes.each do |mng_node|
+        remote_db = mng_node.get_node_db
+        # Catch an event if we cannot contact the server
+        begin 
+          local_node_db.replicate_from(remote_db)
+          # TODO: Manage conflicts here
+          local_node_db.replicate_to(remote_db)
+          break
+        rescue
+          puts "Could not connect to DB node #{mng_node.name}"
+          # TODO: change status or chose another management server
+        end 
+      end
     else
       # Take all the management nodes with the same priority as us
       all_management_nodes = Node.by_is_management(our_node.is_management)
@@ -17,8 +26,13 @@ class NodeDB
       # TODO: Manage conflicts here
       all_management_nodes.each do |mng_node|
          if ! (mng_node.name == our_node.name)
-           local_node_db.replicate_from(mng_node.get_node_db)
-           local_node_db.replicate_to(mng_node.get_node_db)
+           begin
+             local_node_db.replicate_from(mng_node.get_node_db)
+             local_node_db.replicate_to(mng_node.get_node_db)
+           rescue
+             puts "Could not connect to DB node #{mng_node.name}"
+             # TODO: change status or chose another management server             
+           end
          end 
       end
     end
