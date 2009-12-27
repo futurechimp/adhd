2a384d40ad9e140bd9203b3ee580d40ed9fbbc78
dave@netbook <dave@netbook>
2009-12-27 17:53:30 +0000

    Ditching some test code that we've moved into a specific unit test.

--- test/test_adhd.rb
+++ test/test_adhd.rb
@@ -30,44 +30,5 @@ class TestAdhd <  Test::Unit::TestCase
     end
   end
 
-
-  context "The Node model" do
-    setup do
-      @node = Node.new
-    end
-
-    should "have a name property" do
-      assert @node.respond_to? "name"
-    end
-
-    should "have a url property" do
-      assert @node.respond_to? "url"
-    end
-
-    should "have have an is_store property" do
-      assert @node.respond_to? "is_store"
-    end
-
-    should "have an is_management property" do
-      assert @node.respond_to? "is_management"
-    end
-
-    should "have an is_directory property" do
-      assert @node.respond_to? "is_directory"
-    end
-
-    should "have a status property" do
-      assert @node.respond_to? "status"
-    end
-
-    should "have timestamp properties" do
-      assert @node.respond_to? "created_at"
-      assert @node.respond_to? "updated_at"
-    end
-
-  end
-
-  private
-
 end
 
