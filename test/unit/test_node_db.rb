aca945f575a6068cdf2f06272f23fcb8b80c7030
dave@netbook <dave@netbook>
2009-12-29 17:32:13 +0000

    Adding some return statements to make the code do what it's supposed to (still seems to work test-wise).

--- test/unit/test_node_db.rb
+++ test/unit/test_node_db.rb
@@ -7,7 +7,7 @@ class Node
   def initialize event_list
     @event_list = event_list
   end
-  
+
   attr_accessor :status, :name, :is_management
 
   def self.set_nodes node_list
@@ -22,29 +22,29 @@ class Node
   def get_node_db
     @name
   end
-  
+
   def replicate_to(local_db, other_node, remote_db)
-    false if other_node.status == "UNAVAILABLE" or other_node.name == name
+    return false if other_node.status == "UNAVAILABLE" or other_node.name == name
     @event_list << [:rep, local_db, remote_db]
     true
   end
-  
+
   def replicate_from(local_db, other_node, remote_db)
-    false if other_node.status == "UNAVAILABLE" or other_node.name == name
+    return false if other_node.status == "UNAVAILABLE" or other_node.name == name
     @event_list << [:rep, remote_db, local_db]
     true
   end
-  
+
 end
 
 class TestNodeDb <  Test::Unit::TestCase
 
   def get_random_node
-    random_log = @node_log.sort_by { rand }      
+    random_log = @node_log.sort_by { rand }
     target_node = random_log[0]
-   
+
     while  target_node.status == "UNAVAILABLE"
-      random_log = random_log.sort_by { rand }      
+      random_log = random_log.sort_by { rand }
       target_node = random_log[0]
     end
     target_node
@@ -56,7 +56,7 @@ class TestNodeDb <  Test::Unit::TestCase
       # Make a CouchDB node_db and map the Node object there
       @event_log = []
       @node_log = []
-      100.times do |i|        
+      100.times do |i|
         n = Node.new @event_log
         if i < 5
           n.is_management = 3
@@ -67,17 +67,17 @@ class TestNodeDb <  Test::Unit::TestCase
         else
           n.status = "RUNNING"
         end
-        @node_log << n  
+        @node_log << n
       end
-      
+
       Node.set_nodes @node_log
-      
+
     end
-    
+
     should "return some management nodes (even when fake)" do
       assert Node.by_is_management.length > 0
     end
-    
+
     should "sync to a management node (PROBABILISTIC)" do
       old_log = @node_log.clone
       target_node = nil
@@ -85,49 +85,50 @@ class TestNodeDb <  Test::Unit::TestCase
         target_node = get_random_node
       end
       ndb = NodeDB.new(target_node)
-      
-      ndb.sync     
+
+      ndb.sync
       # Two events should fire up -- a sync to and from the server
       assert @event_log.length >= 2
       assert (@event_log.find {|log| old_log[log[2]].is_management}).length > 0
-      assert (@event_log.find {|log| old_log[log[1]].is_management}).length > 0   
+      assert (@event_log.find {|log| old_log[log[1]].is_management}).length > 0
     end
-    
+
     should "sync eventually to all" do
-      # Ok this is going to be insane    
+      # Ok this is going to be insane
       # We test that eventually all nodes get some info
       first_node = nil
       1000.times do |i|
         target_node = get_random_node
-        first_node = target_node if !first_node 
+        first_node = target_node if !first_node
         ndb = NodeDB.new(target_node)
         ndb.sync
       end
-      
+
       #Now we want to show that all running nodes go the information
       # if they called sync after the node updated
-    
+
       tainted = {}
       @node_log.each do |n|
         tainted[n.name] = false
       end
-      
+
       tainted[first_node.name] = true
       @event_log.each do |ev|
         from_node = ev[1]
         to_node = ev[2]
-        tainted[to_node] |= tainted[from_node] 
+        tainted[to_node] |= tainted[from_node]
       end
-      
+
       # @node_log.each do |n|
       #  puts "#{n.name}: #{tainted[n.name]} (#{n.status})"
       # end
-      
+
       assert @node_log.all? {|n| (n.status == "UNAVAILABLE") or tainted[n.name]}
-      
+
     end
-    
-    
+
+
   end
 
 end
+
