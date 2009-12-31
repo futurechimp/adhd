c883c9cd2d906896c14f452d18ba6afc0236847b
dave@netbook <dave@netbook>
2009-12-31 00:19:00 +0000

    Namespaced the rest server a bit more nicely.

--- lib/adhd/rest_server.rb
+++ lib/adhd/rest_server.rb
@@ -3,207 +3,210 @@ require 'uri'
 require 'net/http'
 require 'webrick'
 
-# This implements the connection that proxies an incoming file to a
-# CouchDB instance, as an attachment.  We stream incoming files so that
-# we don't need to buffer them in memory (which would be psychotic for large
-# files) or write them to disk as temp storage (eating up disk space and
-# causing lag).
-#
-module CouchStreamerProxy
+module Adhd
 
-  def initialize our_client_conn, init_request
-    @our_client_conn = our_client_conn
-    @init_request = init_request
-    our_client_conn.proxy_conn = self
-  end
-
-  # We have opened a connection to the CouchDB server, so now it is time
-  # to send the initial Couch request, using HTTP 1.0.
+  # This implements the connection that proxies an incoming file to a
+  # CouchDB instance, as an attachment.  We stream incoming files so that
+  # we don't need to buffer them in memory (which would be psychotic for large
+  # files) or write them to disk as temp storage (eating up disk space and
+  # causing lag).
   #
-  def post_init
-     send_data @init_request
+  module CouchStreamerProxy
+
+    def initialize our_client_conn, init_request
+      @our_client_conn = our_client_conn
+      @init_request = init_request
+      our_client_conn.proxy_conn = self
+    end
+
+    # We have opened a connection to the CouchDB server, so now it is time
+    # to send the initial Couch request, using HTTP 1.0.
+    #
+    def post_init
+       send_data @init_request
+    end
+
+    def receive_data data
+      @our_client_conn.proxy_receive_data data
+    end
+
+    def unbind
+      @our_client_conn.proxy_unbind
+    end
+
   end
 
-  def receive_data data
-    @our_client_conn.proxy_receive_data data
-  end
+  module RestServer
 
-  def unbind
-    @our_client_conn.proxy_unbind
-  end
+    attr_accessor :proxy_conn
 
-end
+    def initialize node_manager
+      @node_manager = node_manager
+      @buffer = ""
+      @status = :header
+    end
 
-module AdhdRESTServer
+     #def post_init
+     #  puts "-- someone connected to the echo server!"
+     #end
 
-  attr_accessor :proxy_conn
+    # The format we are expecting:
+    #
+    # PUT somedatabase/document/attachment?rev=123 HTTP/1.0
+    # Content-Length: 245
+    # Content-Type: image/jpeg
+    #
+    # <JPEG data>
+    #
+    def receive_data data
 
-  def initialize node_manager
-    @node_manager = node_manager
-    @buffer = ""
-    @status = :header
-  end
+       # First we get all the headers in to find out which resource
+       # we are looking for.
 
-   #def post_init
-   #  puts "-- someone connected to the echo server!"
-   #end
+      if @status == :header
+        @buffer += data
 
-  # The format we are expecting:
-  #
-  # PUT somedatabase/document/attachment?rev=123 HTTP/1.0
-  # Content-Length: 245
-  # Content-Type: image/jpeg
-  #
-  # <JPEG data>
-  #
-  def receive_data data
+        if data =~ /\r\n\r\n/
 
-     # First we get all the headers in to find out which resource
-     # we are looking for.
+          # Detected end of headers
+          header_data = @buffer[0...($~.begin(0))]
 
-    if @status == :header
-      @buffer += data
+          @web_config = WEBrick::Config::HTTP.clone
+          @web_config[:HTTPVersion] = WEBrick::HTTPVersion.new("1.0")
 
-      if data =~ /\r\n\r\n/
+          # Try the webrick parser
+          @req = WEBrick::HTTPRequest.new(@web_config)
+          @res = WEBrick::HTTPResponse.new(@web_config)
 
-        # Detected end of headers
-        header_data = @buffer[0...($~.begin(0))]
+          StringIO.open(header_data, 'rb') do |socket|
+            @req.parse(socket)
+          end
 
-        @web_config = WEBrick::Config::HTTP.clone
-        @web_config[:HTTPVersion] = WEBrick::HTTPVersion.new("1.0")
+          # The rest of the incomming connection
+          @buffer = @buffer[($~.end(0))..-1]
 
-        # Try the webrick parser
-        @req = WEBrick::HTTPRequest.new(@web_config)
-        @res = WEBrick::HTTPResponse.new(@web_config)
+          # Compute the ID of the sought resource
+          if @req.path =~ /\/adhd\/(.*)/
+            @req.header["Filename"] = $1
+            @req.header["ID"] = MD5.new($1).to_s
+          else
+            # TODO: return a 404 here
+            raise "Remember to have a url of the form /adhd/<filenname>."
+          end
 
-        StringIO.open(header_data, 'rb') do |socket|
-          @req.parse(socket)
-        end
-
-        # The rest of the incomming connection
-        @buffer = @buffer[($~.end(0))..-1]
-
-        # Compute the ID of the sought resource
-        if @req.path =~ /\/adhd\/(.*)/
-          @req.header["Filename"] = $1
-          @req.header["ID"] = MD5.new($1).to_s
+          # Change the status once headers are found
+          @status = :find_node
         else
-          # TODO: return a 404 here
-          raise "Remember to have a url of the form /adhd/<filenname>."
+          # Avoid DoS via buffer filling
+          close_connection if @buffer.length > 1000
         end
-
-        # Change the status once headers are found
-        @status = :find_node
-      else
-        # Avoid DoS via buffer filling
-        close_connection if @buffer.length > 1000
       end
-    end
 
-    # Now we have the headers, but maybe not the full body, and we are looking
-    # for the right node in our network to handle the call.
-    if @status == :find_node
-      pause # We want to tell the remote host to wait a bit
-            # This would allow us to defer the execution of the calls to find
-            # the right nodes, and extract the doc.
+      # Now we have the headers, but maybe not the full body, and we are looking
+      # for the right node in our network to handle the call.
+      if @status == :find_node
+        pause # We want to tell the remote host to wait a bit
+              # This would allow us to defer the execution of the calls to find
+              # the right nodes, and extract the doc.
 
-        # TODO: We need to push all the chit-chat with the remote servers to
-        #       A deferable object, or some other connection, not to block.
-        #       Right now we are blocking and it sucks.
+          # TODO: We need to push all the chit-chat with the remote servers to
+          #       A deferable object, or some other connection, not to block.
+          #       Right now we are blocking and it sucks.
 
-        # Now get or write the document associated with this file
+          # Now get or write the document associated with this file
 
-      if @req.request_method == "GET"
+        if @req.request_method == "GET"
 
-        @our_doc = @node_manager.srdb.get_doc_directly(@req.header["ID"])
+          @our_doc = @node_manager.srdb.get_doc_directly(@req.header["ID"])
 
-        # TODO: handle errors if file does not exist
-        if @our_doc[:ok]
-          @status == :get
-            handle_get
-          else
-            send_data "Problem"
-            close_connection
-          end
-        end
-
-        if @req.request_method == "PUT"
-          # Define a Doc with the data so far
-          @our_doc = StoredFile.new
-
-          @our_doc._id = @req.header["ID"]
-          @our_doc.internal_id = @req.header["ID"]
-          @our_doc.size_bytes = @req.content_length
-          @our_doc.filename = @req.header["Filename"]
-          @our_doc.mime_type = @req.content_type
-
-          # Write to the right node
-          @our_doc = @node_manager.srdb.write_doc_directly(@our_doc)
-
-          # TODO: if an error is returned here, we cannot execute the query
+          # TODO: handle errors if file does not exist
           if @our_doc[:ok]
-            @status = :put
-            handle_put
-          else
-            send_data "Problem"
-            close_connection
+            @status == :get
+              handle_get
+            else
+              send_data "Problem"
+              close_connection
+            end
+          end
+
+          if @req.request_method == "PUT"
+            # Define a Doc with the data so far
+            @our_doc = StoredFile.new
+
+            @our_doc._id = @req.header["ID"]
+            @our_doc.internal_id = @req.header["ID"]
+            @our_doc.size_bytes = @req.content_length
+            @our_doc.filename = @req.header["Filename"]
+            @our_doc.mime_type = @req.content_type
+
+            # Write to the right node
+            @our_doc = @node_manager.srdb.write_doc_directly(@our_doc)
+
+            # TODO: if an error is returned here, we cannot execute the query
+            if @our_doc[:ok]
+              @status = :put
+              handle_put
+            else
+              send_data "Problem"
+              close_connection
+            end
           end
         end
       end
+
+    def handle_get
+      resume
+      # We need to connect to the right server and build a header
+      server_uri = URI.parse(@our_doc[:db].server.uri)
+      server_addr = server_uri.host
+      server_port = server_uri.port
+
+      docid = @our_doc[:doc]._id
+      dbname = @our_doc[:db].name
+      request = "GET /#{dbname}/#{docid}/#{@our_doc[:doc].filename} HTTP/1.0\r\n\r\n"
+      #send_data request
+      #close_connection_after_writing
+      puts "Connect to #{server_addr} port #{server_port}"
+      puts "#{request}"
+      conn = EM::connect server_addr, server_port, CouchStreamerProxy, self, request
+      EM::enable_proxy proxy_conn, self, 1024*10
     end
 
-  def handle_get
-    resume
-    # We need to connect to the right server and build a header
-    server_uri = URI.parse(@our_doc[:db].server.uri)
-    server_addr = server_uri.host
-    server_port = server_uri.port
+    def proxy_unbind
+      # Our connection to the CouchDB has just been torn down
+      close_connection_after_writing
+    end
 
-    docid = @our_doc[:doc]._id
-    dbname = @our_doc[:db].name
-    request = "GET /#{dbname}/#{docid}/#{@our_doc[:doc].filename} HTTP/1.0\r\n\r\n"
-    #send_data request
-    #close_connection_after_writing
-    puts "Connect to #{server_addr} port #{server_port}"
-    puts "#{request}"
-    conn = EM::connect server_addr, server_port, CouchStreamerProxy, self, request
-    EM::enable_proxy proxy_conn, self, 1024*10
+    def proxy_receive_data data
+      # Response to a PUT request only
+      send_data data
+    end
+
+    def handle_put
+      resume
+
+      # We need to connect to the right server and build a header
+      server_uri = URI.parse(@our_doc[:db].server.uri)
+      server_addr = server_uri.host
+      server_port = server_uri.port
+
+      docid = @our_doc[:doc]._id
+      dbname = @our_doc[:db].name
+      request = "PUT /#{dbname}/#{docid}/#{@our_doc[:doc].filename}?rev=#{@our_doc[:doc]["_rev"]} HTTP/1.0\r\n"
+      request += "Content-Type: #{@our_doc[:doc].mime_type}\r\n"
+      request += "Content-Length: #{@our_doc[:doc].size_bytes}\r\n"
+      request += "\r\n"
+      request += @buffer
+      #send_data request
+      #close_connection_after_writing
+      # puts "Connect to #{server_addr} port #{server_port}"
+      conn = EM::connect server_addr, server_port, CouchStreamerProxy, self, request
+      EM::enable_proxy self, proxy_conn, 1024 * 10
+    end
+
+    def unbind
+      # puts "-- someone disconnected from the echo server!"
+    end
   end
-
-  def proxy_unbind
-    # Our connection to the CouchDB has just been torn down
-    close_connection_after_writing
-  end
-
-  def proxy_receive_data data
-    # Response to a PUT request only
-    send_data data
-  end
-
-  def handle_put
-    resume
-
-    # We need to connect to the right server and build a header
-    server_uri = URI.parse(@our_doc[:db].server.uri)
-    server_addr = server_uri.host
-    server_port = server_uri.port
-
-    docid = @our_doc[:doc]._id
-    dbname = @our_doc[:db].name
-    request = "PUT /#{dbname}/#{docid}/#{@our_doc[:doc].filename}?rev=#{@our_doc[:doc]["_rev"]} HTTP/1.0\r\n"
-    request += "Content-Type: #{@our_doc[:doc].mime_type}\r\n"
-    request += "Content-Length: #{@our_doc[:doc].size_bytes}\r\n"
-    request += "\r\n"
-    request += @buffer
-    #send_data request
-    #close_connection_after_writing
-    # puts "Connect to #{server_addr} port #{server_port}"
-    conn = EM::connect server_addr, server_port, CouchStreamerProxy, self, request
-    EM::enable_proxy self, proxy_conn, 1024 * 10
-  end
-
-   def unbind
-     # puts "-- someone disconnected from the echo server!"
-   end
- end
+end
 
